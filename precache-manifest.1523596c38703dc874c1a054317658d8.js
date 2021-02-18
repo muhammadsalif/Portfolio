f56def78ca7261a44ee04baf1b9f14e4a1b26bfe
muhammadsalif <muhammad.salif@yahoo.com>
2021-02-18 08:14:59 +0000

    Deploying to gh-pages from  @ 6d70b289bb783a13dcdb7b3890405b85c7a1d1bc 🚀

---
+++ precache-manifest.1523596c38703dc874c1a054317658d8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3837c75fbb5f459c2a83aba6963bb6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06685eb6545a665cdb4e",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "73908d77e8ebb21c6aae",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "251571085478aa777475",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "0da6a26dd7785c2c0623",
+    "url": "/static/js/2.3a6459ae.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.3a6459ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06685eb6545a665cdb4e",
+    "url": "/static/js/3.cef10a75.chunk.js"
+  },
+  {
+    "revision": "73908d77e8ebb21c6aae",
+    "url": "/static/js/4.5535bc08.chunk.js"
+  },
+  {
+    "revision": "251571085478aa777475",
+    "url": "/static/js/main.3ddc71e1.chunk.js"
+  },
+  {
+    "revision": "c6ccf4bfde97184ad11b",
+    "url": "/static/js/runtime-main.dc8b3f1a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "c9e180b302e90aa6081313a018243b4c",
+    "url": "/static/media/dataForzeLogo.c9e180b3.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "fa4b5389bbeb8dc7153de6aa071f824b",
+    "url": "/static/media/kuLogo.fa4b5389.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "5e6abd202b5f8df977bb0a02853452a8",
+    "url": "/static/media/sysborg-logo.5e6abd20.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
